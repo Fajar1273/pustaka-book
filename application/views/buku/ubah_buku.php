b2874eb4082a50bf6d32802d277431fd5e5aace4
Fajar1273 <fajarrizki809@gmail.com>
2023-11-08 14:28:54 +0700

    add form ubah buku

--- application/views/buku/ubah_buku.php
+++ application/views/buku/ubah_buku.php
@@ -1,66 +1,110 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-12">
+			<?= form_open_multipart(`buku/ubahBuku/$id`); ?>
+			<?php foreach ($buku as $b) { ?>
+				<input type="text" class="form-control" hidden name="id" name="judul_buku" value="<?= $b['id']; ?>">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="form-group">
+							<i class="fas fa-book"></i>
+							<label for="judul">Judul</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="judul" name="judul_buku"value="<?= $b['judul_buku'] ?>">
+								<?= form_error('judul_buku', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="form-group">
+							<i class="fas fa-book"></i>
+							<label for="id_kategori">Id Kategori</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="id_kategori" name="id_kategori" value="<?= $b['id_kategori'] ?>">
+								<?= form_error('id_kategori', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="form-group">
+							<i class="fas fa-pen"></i>
+							<label for="pengarang">Pengarang</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="pengarang" name="pengarang" value="<?= $b['pengarang'] ?>">
+								<?= form_error('pengarang', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
 
-<!-- /.container-fluid -->
-</div>
-<!-- End of Main Content -->
-<!-- Modal Tambah buku baru-->
-<div class="modal fade" id="bukuBaruModal" tabindex="-1" role="dialog" aria-labelledby="bukuBaruModalLabel" aria-hidden="true">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="bukuBaruModalLabel">Tambah Buku</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="form-group">
+							<i class="fas fa-user-tag"></i>
+							<label for="penerbit">Penerbit</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= $b['penerbit'] ?>">
+								<?= form_error('penerbit', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="form-group">
+							<i class="fas fa-calendar"></i>
+							<label for="tahun">Tahun</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="tahun" name="tahun" value="<?= $b['tahun_terbit'] ?>">
+								<?= form_error('tahun', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="form-group">
+							<i class="fas fa-address-book"></i>
+							<label for="isbn">ISBN</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="isbn" name="isbn" value="<?= $b['isbn'] ?>">
+								<?= form_error('isbn', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="form-group">
+							<i class="fas fa-book-open"></i>
+							<label for="stok">stok</label>
+							<div class="form-isi">
+								<input type="text" class="form-control" id="stok" name="stok" value="<?= $b['stok'] ?>">
+								<?= form_error('stok', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="form-group row">
+					<picture>
+						<source srcset="" type="image/svg+xml">
+						<img src="<?= base_url('assets/img/upload/') . $b['image']; ?>" class="img-fluid img-thumbnail" style="max-width: 35%;" alt="...">
+					</picture>
+					<div class="col-sm-9">
+						<div class="custom-file">
+							<input type="text" hidden name="old_image" value="<?= $b['image']; ?>">
+							<input type="file" class="custom-file-input" id="image" name="image" value="<?= $b['image']; ?>">
+							<label class="custom-file-label" for="image">Pilih file</label>
+						</div>
+					</div>
+				</div>
+				<?php 
+			} ?>
+			<div class="form-group row justify-content-end">
+				<div class="col-sm-10">
+					<button type="submit" class="btn btn-primary">Ubah</button>
+					<button class="btn btn-dark" onclick="window.history.go(-1)"> Kembali</button>
+				</div>
 			</div>
-			<form action="<?= base_url('buku'); ?>" method="post" enctype="multipart/form-data">
-				<div class="modal-body">
-					<div class="form-group">
-						<input type="text" class="form-control form-control-user" id="judul_buku" name="judul_buku"
-							placeholder="Masukkan Judul Buku">
-					</div>
-					<div class="form-group">
-						<select name="id_kategori" class="form-control form-control-user">
-							<option value="">Pilih Kategori</option>
-							<?php
-              foreach ($kategori as $k) { ?>
-                <option value="<?= $k['id_kategori'];?>"><?=$k['nama_kategori'];?></option>
-                <?php 
-              } ?>
-						</select>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control form-control-user" id="pengarang" name="pengarang" placeholder="Masukkan nama pengarang">
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control form-control-user" id="penerbit" name="penerbit" placeholder="Masukkan nama penerbit">
-					</div>
-					<div class="form-group">
-						<select name="tahun" class="form-control form-control-user">
-							<option value="">Pilih Tahun</option>
-							<?php
-              for ($i=date('Y'); $i > 1000 ; $i--) { 
-                ?>
-                <option value="<?= $i;?>"><?=$i;?></option>
-                <?php 
-              } ?>
-						</select>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control form-control-user" id="isbn" name="isbn" placeholder="Masukkan ISBN">
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control form-control-user" id="stok" name="stok" placeholder="Masukkan nominal stok">
-					</div>
-					<div class="form-group">
-						<input type="file" class="form-control form-control-user" id="image" name="image">
-					</div>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
-					<button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Tambah</button>
-				</div>
 			</form>
 		</div>
 	</div>
 </div>
-<!-- End of Modal Tambah Mneu -->
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->

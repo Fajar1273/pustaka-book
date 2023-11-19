0ab9e5f4f1f377bd5b4c278ea62638fa2b82e835
fjrrzki <fajarrizki809@gmail.com>
2023-11-19 19:10:44 +0700

    update views anggota

--- application/views/user/ubah-profile.php
+++ application/views/user/ubah-profile.php
@@ -6,7 +6,8 @@
 			<div class="form-group row">
 				<label for="email" class="col-sm-2 col-form-label">Email</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
+					<input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>"
+						readonly>
 				</div>
 			</div>
 			<div class="form-group row">
@@ -21,7 +22,8 @@
 				<div class="col-sm-10">
 					<div class="row">
 						<div class="col-sm-3">
-							<img src="<?=base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="">
+							<img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail"
+								alt="">
 						</div>
 						<div class="col-sm-9">
 							<div class="custom-file">
@@ -35,7 +37,7 @@
 			<div class="form-group row justify-content-end">
 				<div class="col-sm-10">
 					<button type="submit" class="btn btn-primary">Ubah</button>
-					<button class="btn btn-dark" onclick="window.history.go(-1)"> Kembali</button>
+					<button class="btn btn-dark" onclick="window.history.go(-1); return false;"> Kembali</button>
 				</div>
 			</div>
 			</form>
